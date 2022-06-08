35a95e98f94740b120f6dae68696aeaf227a106a
root 5856 <202101314@bssm.hs.kr>
2022-06-08 20:29:50 +0900

    2022-06-08

--- 강서구/gangs/src/components/slide.jsx
+++ 강서구/gangs/src/components/slide.jsx
@@ -2,20 +2,23 @@ import React from "react";
 import "../App.css";
 import { FullPage, Slide } from "react-full-page";
 import Header from "./header";
+import { AiOutlineArrowDown } from "react-icons/ai";
 
 export default function slide() {
   return (
     <div>
       <FullPage controls controlsProps={{ className: "slide-navigation" }}>
         <Slide>
-          <div>
-            
+          <div className="firstP">
+            <div className="floating">
+              <h2 >갤러리</h2>
+              <AiOutlineArrowDown size="35%" />
+            </div>
           </div>
         </Slide>
-        <Slide>
+        <Slide className="secondP">
           <div>
-            <Header/>
-            
+            <Header />
           </div>
         </Slide>
       </FullPage>

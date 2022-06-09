78f7292d3ffbae10dc884a1d4f8f32a7c439b847
root 5856 <202101314@bssm.hs.kr>
2022-06-09 15:51:19 +0900

    2022-06-09

--- 강서구/gangs/src/components/slide.jsx
+++ 강서구/gangs/src/components/slide.jsx
@@ -10,17 +10,7 @@ export default function slide() {
       <FullPage controls controlsProps={{ className: "slide-navigation" }}>
         <Slide className="firstS">
           <div className="firstP">
-            <div className="firstT">
-              <h1>여기는 h1태그가 들어갑니다</h1>
-              <h3>
-                여기는 h3태그 내용 나는고양이, 나는천재, 나는멍청이가 들어갑니다
-              </h3>
-            </div>
 
-            <div className="floating">
-              <h2>갤러리</h2>
-              <AiOutlineArrowDown size="35%" />
-            </div>
           </div>
         </Slide>
         <Slide className="secondP">

5569f2d721ef00ce10c120cba34f3becfa9ccdff
root 5856 <202101314@bssm.hs.kr>
2022-06-09 15:33:18 +0900

    2022-06-09

--- 강서구/gangs/src/components/slide.jsx
+++ 강서구/gangs/src/components/slide.jsx
@@ -6,17 +6,21 @@ import { AiOutlineArrowDown } from "react-icons/ai";
 
 export default function slide() {
   return (
-    <div >
+    <div>
       <FullPage controls controlsProps={{ className: "slide-navigation" }}>
         <Slide className="firstS">
           <div className="firstP">
-            <h1>여기는 h1태그가 들어갑니다</h1>
+            <div className="firstT">
+              <h1>여기는 h1태그가 들어갑니다</h1>
+              <h3>
+                여기는 h3태그 내용 나는고양이, 나는천재, 나는멍청이가 들어갑니다
+              </h3>
+            </div>
 
-            <div className="floating">  {/* 애니메이션 화살표 */}
+            <div className="floating">
               <h2>갤러리</h2>
               <AiOutlineArrowDown size="35%" />
             </div>
-
           </div>
         </Slide>
         <Slide className="secondP">

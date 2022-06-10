34b37a9c34912eea58df84c24f29ae4552bc0ea8
root 5856 <202101314@bssm.hs.kr>
2022-06-11 00:52:10 +0900

    2022-06-11

--- 강서구/gangs/src/components/slide.jsx
+++ 강서구/gangs/src/components/slide.jsx
@@ -1,27 +1,46 @@
 import React from "react";
 import "../App.css";
-import { FullPage, Slide } from "react-full-page";
 import Header from "./header";
 
 export default function slide() {
   return (
     <div>
-      <FullPage controls controlsProps={{ className: "slide-navigation" }}>
-        <Slide className="firstS">
-          <div className="firstP">
-            <div className="firstE">
-                <h1>강서구 알리기</h1>
-                <p>줄바꿈 왜 안되냐</p>
-                
-            </div>
-          </div>
-        </Slide>
-        <Slide className="secondP">
-          <div>
-            <Header />
-          </div>
-        </Slide>
-      </FullPage>
+      <div className="p1">
+        
+      </div>
+      <div className="p2">
+        
+      </div>
+      <div className="p3">
+        
+      </div>
+      <div className="p4">
+        
+      </div>
+      <div className="p5">
+        
+      </div>
+      <div className="p6">
+        
+      </div>
+      <div className="p7">
+        
+      </div>
+      <div className="p8">
+        
+      </div>
+      <div className="p9">
+        
+      </div>
+      <div className="p10">
+        
+      </div>
+      <div className="p11">
+        
+      </div>
+      <div className="p12">
+        
+      </div>
     </div>
   );
 }

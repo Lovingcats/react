c1de8518a4a4fdc5f34beecbda9f8d1137ab5eab
root 5856 <whehdrms6810@naver.com>
2022-06-16 17:02:56 +0900

    dicegame logo

---
+++ dicegame/dicegame/src/components/header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./header.scss";
+
+export default function header() {
+  return (
+    <div id="dice">
+      <div className="box">
+        <span className="w1">p</span>
+        <span className="w2">l</span>
+        <span className="w3">a</span>
+        <span className="w4">y</span>
+        <span className="w5">i</span>
+        <span className="w6">t</span>
+      </div>
+    </div>
+  );
+}

a5d8434ad5727870cc313901dccfd5a483796f7c
root 5856 <whehdrms6810@naver.com>
2022-06-21 20:03:04 +0900

    2202-06-21

--- dicegame/dicegame/src/components/play.jsx
+++ dicegame/dicegame/src/components/play.jsx
@@ -5,12 +5,7 @@ export default function play() {
   return (
     <div id="dice1">
       <div className="box1">
-        <span className="d1">1</span>
-        <span className="d2">2</span>
-        <span className="d3">3</span>
-        <span className="d4">4</span>
-        <span className="d5">5</span>
-        <span className="d6">6</span>
+        시작하기
       </div>
     </div>
   );

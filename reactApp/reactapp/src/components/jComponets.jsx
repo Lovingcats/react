4203979b4705c98ae1aa592f7daa99830ca1d48b
조용제 <whehdrms6810@naver.com>
2022-12-21 21:33:01 +0900

    [CREATE] jComponets.jsx

---
+++ reactApp/reactapp/src/components/jComponets.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const jComponetns = (children) => {
+    return (
+      <div style={{border: "1px solid black"}}>
+          <span>J</span>
+          {children}
+      </div>
+    )
+  }
+
